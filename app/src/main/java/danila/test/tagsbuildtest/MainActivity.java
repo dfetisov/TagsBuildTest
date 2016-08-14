7216c00134130924fbe75c37f0b6d44d1b4b5569
Danila Fetisov <dfetisov@gradoservice.ru>
2016-08-14 21:47:19 +0300

    Initial commit

---
+++ app/src/main/java/danila/test/tagsbuildtest/MainActivity.java
@@ -0,0 +1,13 @@
+package danila.test.tagsbuildtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
